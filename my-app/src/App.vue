ddcfdba12bde3631fd2b4f498b5c6f5257e97504
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-06 10:13:35 +0200

    add my first vue app:  my-app

---
+++ my-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld
+      v-for="message of messages"
+      v-bind:key="message.id"
+      v-bind:msg="message.content"
+      class="display">{{message.id}}</HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      messages: [{id: 1, content: "Hello world!"},
+      {id: 2, content: "I'm hungry"},
+      {id: 3, content: "I mean, i'm always hungry"}]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.display {
+  text-align: center;
+}
+</style>
---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    console.log(msg.id : <slot></slot>)
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  text-shadow: #42b883 3px 2px;
+  /* background-color: magenta; */
+}
+</style>
