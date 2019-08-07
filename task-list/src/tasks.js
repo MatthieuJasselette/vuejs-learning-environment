5620a3bb9e0ed1647325dfeca2e1d39cd732d00a
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-07 10:00:08 +0200

    rework tasks class some more, add default export

--- task-list/src/tasks.js
+++ task-list/src/tasks.js
@@ -1,9 +1,9 @@
 const uuidv4 = require('uuid/v4');
 
-export class Tasks {
-    constructor(inputTitle){
-        this.id = uuidv4
-        this.title = inputTitle
+export default class Tasks {
+    constructor(){
+        this.id = uuidv4()
+        this.title = "Make a task list"
         this.isCompleted = false
     }
 }
--- task-list/src/tasks.ts
+++
@@ -1,7 +0,0 @@
-const uuidv4 = require('uuid/v4');
-
-export class Tasks {
-    id: String = uuidv4
-    title: String
-    isCompleted: Boolean
-}
