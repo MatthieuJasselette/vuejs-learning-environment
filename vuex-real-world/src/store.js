6d937c986028e6b56a538fa1e88f81257c0c76db
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-08 15:45:47 +0200

    healthy changes

--- vuex-real-world/src/store.js
+++ vuex-real-world/src/store.js
@@ -1,10 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Adam Jahr'
+    },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      return state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+      commit('ADD_EVENT', event)
+    }
+  },
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
