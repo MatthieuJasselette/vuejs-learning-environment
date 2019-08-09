2911c9a4a6cf383f643066de76febdd4cad3527c
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-09 13:30:39 +0200

    refactor getEvents to work through the store & add pagination to eventlist

--- vuex-real-world/src/views/EventList.vue
+++ vuex-real-world/src/views/EventList.vue
@@ -1,31 +1,33 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link :to="{name: 'event-list', query:{ page: page - 1 } }" rel="prev">Previous page</router-link>|
+    </template>
+    <router-link :to="{name: 'event-list', query:{ page: page + 1 } }" rel="next">Next page</router-link>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error:', error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: '3',
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events'])
   }
 }
 </script>
